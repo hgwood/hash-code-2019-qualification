42f35129136159a0ffebdbc32a7e54c3a14bdd60
Hugo Wood <hx4.5@free.fr>
2019-02-28 19:04:02 +0100

    :bulb: jsdoc for input and output of solve

--- solve.js
+++ solve.js
@@ -2,6 +2,23 @@ const debug = require("debug")("solve");
 const _ = require("lodash");
 const gridUtils = require("./grid-utils");
 
+/**
+ * @typedef {object} Photo
+ * @property {"H" | "V"} orientation
+ * @property {Array<String>} tags
+ *
+ * @typedef {object} PhotoCollection
+ * @property {Array<Photo>} photos
+ *
+ * @typedef {Array<[number] | [number, number]>} Slideshow
+ */
+
+/**
+ *
+ * @param {PhotoCollection} problem
+ * @param {string} file
+ * @returns {Slideshow}
+ */
 function solve(problem, file) {
   // destructure this!
   return [];
