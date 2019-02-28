573c4fe58544dc2203030129ef0ddcde4a9a50de
Errorname <do.not.press.enter@gmail.com>
2019-02-28 19:56:12 +0100

    :bento: JSON in with index

--- photos-to-id.js
+++
@@ -1,7 +0,0 @@
-const assert = require("assert");
-const debug = require("debug")("photosToId");
-const _ = require("lodash");
-
-module.exports = function photosToId(photos) {
-  return photos.map(p => p.index);
-};
