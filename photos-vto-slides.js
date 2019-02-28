1d7de27e4216eccecd572dfb6c90fac32fc41a9c
Errorname <do.not.press.enter@gmail.com>
2019-02-28 19:42:53 +0100

    tayo!

--- photos-vto-slides.js
+++ photos-vto-slides.js
@@ -3,5 +3,7 @@ const debug = require("debug")("photosVtoSlides");
 const _ = require("lodash");
 
 module.exports = function photosVtoSlides(photos) {
-  return _.chunk(photos.map(p => p.index), 2);
+  const chunks = _.chunk(photos.map(p => p.index), 2);
+  if (chunks.length > 0 && chunks[chunks.length - 1].length == 1) chunks.pop();
+  return chunks;
 };
